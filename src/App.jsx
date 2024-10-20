6ed42fca6da25ddbdff3d776a48710c73ed7c3fb
hambali <subastianhambali@gmail.com>
2024-10-20 13:59:49 +0700

    fix color

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ function App() {
         <div class="mx-auto max-w-screen-xl px-4 2xl:px-4 w-full">
           <div class="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
             <div className="">
-              <h2 class="mt-3 text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+              <h2 class="mt-3 text-xl font-semibold text-black sm:text-2xl">
                 Hasil Download:
               </h2>
             </div>
